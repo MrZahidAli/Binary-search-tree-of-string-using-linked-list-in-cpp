2a4591e5be35e288e58cdc65f8ec9fb6509332a9
MrZahidAli <46484008+MrZahidAli@users.noreply.github.com>
2019-11-30 13:39:48 +0500

    reading data from file as a string and making BST

--- create BST of string v1.2.cpp
+++ create BST of string v1.2.cpp
@@ -1,28 +1,26 @@
-//(BST) Binary search tree using linked list
-
-// 1- creating BST of string using linked list in cpp
-// 2- printing BST: inorder, preorder, postorder
 
 #include<iostream>
+#include<fstream>
+#include<windows.h>
 using namespace std;
 class node{
 	public:
 		string data;
 		node *left, *right, *previous;
 };
-class linkList{
+class BST{
 	public:
 		node *root;
 	public:
-		linkList(){
+		BST(){
 			root = NULL;
 		}
 //compare strings
 	int biggerString(string str1, string str2){
-		int com = str1.compare(str2);
+		int com = str1.compare(str2);				//return 0 if both are same and 1 if str2 is bigger and -1 if str1 is bigger
 		return com;
 	}
-// 1- creating BST
+// creating BST of string (from file data).
 		void creatList(string value){
 			node *temp = new node;
 			node *copyRoot = root;
@@ -82,10 +80,18 @@ class linkList{
 		}
 };
 int main(){
-	linkList B;
-	B.creatList("zahid");
-	B.creatList("aahid");
-	B.creatList("bahid");
+	system("color 84");
+	BST B;
+	
+	string line;
+	ifstream mfile("spellDic.txt");
+	if (mfile.is_open()){
+		while(getline(mfile,line)){
+    		B.creatList(line);
+    	}
+    	mfile.close();
+	}
+	else cout << "Unable to open file";
 	B.printInorder(B.root);
 	return 0;
 }
